20c7a140c388ecc1883d12d1842dc0e7a2677f8f
Mark Alfaro <mark.alfaro@adventa.mx>
2017-01-05 14:03:50 -0600

    Modifica el endpoint para habilitar/deshabilitar de canje

--- src/Repository/CoreCheckoutActivationLogRepository.php
+++ src/Repository/CoreCheckoutActivationLogRepository.php
@@ -9,15 +9,16 @@
 namespace Adteam\Core\Checkout\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Adteam\Core\Checkout\Entity\CoreConfigs;
 use Adteam\Core\Checkout\Entity\OauthUsers;
 use Adteam\Core\Checkout\Entity\CoreCheckoutActivationLog;
 
 class CoreCheckoutActivationLogRepository extends EntityRepository
 {
-   
+
     /**
      * Fetch all config values
-     * 
+     *
      * @return array
      */
     public function fetchLast()
@@ -33,12 +34,12 @@ class CoreCheckoutActivationLogRepository extends EntityRepository
             return $result[0];
         }
            throw new \InvalidArgumentException(
-                'Inicializar_canje'); 
+                'Inicializar_canje');
     }
-    
+
     /**
-     * verifica si esta vacio log 
-     * 
+     * verifica si esta vacio log
+     *
      * @return boolean
      */
     private function hasInit()
@@ -47,48 +48,73 @@ class CoreCheckoutActivationLogRepository extends EntityRepository
         $result = $this->createQueryBuilder('B')
            ->select("B.id, B.createdAt, R.id as userId, R.displayName, B.status")
            ->innerJoin('B.requestedBy', 'R')
-           ->getQuery()->getResult();  
+           ->getQuery()->getResult();
         if(count($result)<=0){
             $isInit = true;
         }
         return $isInit;
-        
+
     }
 
     /**
-     * 
-     * @param type $data
-     * @param type $identity
+     * @param object $data
+     * @param string $identity
      * @return boolean
      * @throws \InvalidArgumentException
      */
-    public function create($data,$identity)
+    public function create($data, $identity)
     {
         $user = $this->getUserByUsername($identity['user_id']);
-        $enabled = (boolean)$data->enabled;
-        if($this->hasInit()){
-            if($enabled===TRUE){
-                $this->insertLog($user, true);
-                return true; 
-            }else{
+
+        // Obtener los parámetros
+        $dateStart = $data->{"date.start"};
+        $dateEnd = $data->{"date.end"};
+
+        // Al menos un parámetro debe existir
+        if (is_null($dateStart) && is_null($dateEnd)) {
+            throw new \InvalidArgumentException('Missing_Arguments');
+        }
+
+        // Validar rangos correctos
+        if (!is_null($dateStart) && !is_null($dateEnd) && (int)$dateStart >= (int)$dateEnd) {
+            throw new \InvalidArgumentException('Invalid_Range');
+        }
+
+        $currentTime = time();
+
+        // Determinar si el canje será activado (dateStart enviado y fecha actual mayor o igual a fecha de canje)
+        $enabled = !is_null($dateStart) && $currentTime >= $dateStart;
+        $enabled = $enabled && (is_null($dateEnd) ? true : $currentTime < $dateEnd);
+
+        if ($this->hasInit()) {
+            if ($enabled) {
+                return $this->insertLog($user, $dateStart, $dateEnd);
+            } else {
                 throw new \InvalidArgumentException('only_true');
             }
-        }else{
-            $lastcreated = $this->fetchLast();
-            if($enabled===$lastcreated['enabled']){
-               $activo = $lastcreated['enabled']?'canje activo':'canje cerrado';
-               throw new \InvalidArgumentException($activo);                
+        } else {
+            // Validar rango de canje activo
+            $config = $this->_em->getRepository(CoreConfigs::class)->getCheckoutRange();
+
+            $rangeStart = isset($config['checkout.date.start']) ? (int)$config['checkout.date.start'] : PHP_INT_MAX;
+            $rangeEnd = isset($config['checkout.date.end']) ? (int)$config['checkout.date.end'] : 0;
+
+            $checkoutIsActive = $rangeStart <= $currentTime && $currentTime <= $rangeEnd;
+
+            if ($checkoutIsActive === $enabled) {
+                $activo = $checkoutIsActive ? 'canje activo' : 'canje cerrado';
+                throw new \InvalidArgumentException($activo);
             }
-            $this->insertLog($user, $enabled);
-            return $enabled; 
+
+            return $this->insertLog($user, $dateStart, $dateEnd);
         }
     }
 
     /**
      * Obtiene usuario mediante username
-     * 
-     * @param type $username
-     * @return type
+     *
+     * @param string $username
+     * @return array|mixed
      */
     public function getUserByUsername($username)
     {
@@ -99,42 +125,51 @@ class CoreCheckoutActivationLogRepository extends EntityRepository
                 ->setParameter('username', $username)
                 ->getQuery()->getSingleResult();
     }
-    
+
     /**
      * Inserta log de apertura y cierre
      * de canje
-     * 
-     * @param type $user
-     * @param type $status
+     *
+     * @param string $user
+     * @param int $dateStart
+     * @param int $dateEnd
      */
-    public function insertLog($user,$status)
-            
+    public function insertLog($user, $dateStart = null, $dateEnd = null)
     {
         $currentRepo = $this;
-        $this->_em->transactional(
-            function ($em) use($status,$user,$currentRepo) {
+        return $this->_em->transactional(
+            function ($em) use ($dateStart, $dateEnd, $user, $currentRepo) {
                 $log = new CoreCheckoutActivationLog();
-                $UserReference = $em->getReference(
-                            OauthUsers::class, $user['id']);
+                $UserReference = $em->getReference(OauthUsers::class, $user['id']);
+
+                $status = (!is_null($dateStart) || $dateStart <= time())
+                    && (!is_null($dateEnd) || time() < $dateEnd);
+
                 $log->setRequestedBy($UserReference);
                 $log->setStatus($status);
-                $em->persist($log);             
-                $currentRepo->updateCheckoutEnabled($status);
+                $em->persist($log);
+                $currentRepo->updateCheckoutEnabled($dateStart, $dateEnd);
+
+                return $status;
             }
         );
     }
-    
+
     /**
      * update estatus core config
-     * 
-     * @param type $status
-     * @return type
+     *
+     * @param int $dateStart
+     * @param int $dateEnd
+     * @return array|mixed
      */
-    private function updateCheckoutEnabled($status)
-    {    
-        $s = (int)$status;
-        $dql ="UPDATE Adteam\Core\Checkout\Entity\CoreConfigs U SET U.value = '"
-                . $s."' WHERE U.key like 'checkout.enabled'";
-        return $this->_em->createQuery($dql)->getResult();
+    private function updateCheckoutEnabled($dateStart, $dateEnd)
+    {
+        $dql ="UPDATE Adteam\Core\Checkout\Entity\CoreConfigs U SET U.value = "
+                . $dateStart." WHERE U.key like 'checkout.date.start'";
+        $this->_em->createQuery($dql)->execute();
+
+        $dql ="UPDATE Adteam\Core\Checkout\Entity\CoreConfigs U SET U.value = "
+                . $dateEnd." WHERE U.key like 'checkout.date.end'";
+        $this->_em->createQuery($dql)->execute();
     }
 }
